7cfeead419fe1e84bc9370e157ff445c083a8d98
Stephen Celis <stephen@stephencelis.com>
2024-12-20 22:57:09 -0800

    wip

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
   name: "swift-structured-queries",
   platforms: [
-    .iOS(.v15),
-    .macOS(.v12),
-    .tvOS(.v15),
-    .watchOS(.v8),
+    .iOS(.v14),
+    .macOS(.v11),
+    .tvOS(.v14),
+    .watchOS(.v7),
   ],
   products: [
     .library(
