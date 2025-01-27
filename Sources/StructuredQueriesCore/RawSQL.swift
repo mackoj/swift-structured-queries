ef9897eae4c283a5ac430860ff3e616fc0025799
Stephen Celis <stephen@stephencelis.com>
2025-01-26 18:31:54 -0800

    wip

---
+++ Sources/StructuredQueriesCore/RawSQL.swift
@@ -0,0 +1,34 @@
+extension QueryExpression {
+  public static func raw<QueryOutput>(
+    _ queryFragment: QueryFragment,
+    as output: QueryOutput.Type = QueryOutput.self
+  ) -> Self
+  where Self == RawQueryExpression<QueryOutput> {
+    Self(queryFragment: queryFragment)
+  }
+
+  public static func raw<Strategy: QueryBindingStrategy>(
+    _ queryFragment: QueryFragment,
+    as strategy: Strategy
+  ) -> Self
+  where Self == RawQueryExpression<Bind<Strategy>> {
+    Self(queryFragment: queryFragment)
+  }
+}
+
+public struct RawQueryExpression<QueryOutput>: QueryExpression {
+  public let queryFragment: QueryFragment
+}
+
+extension RawQueryExpression: _OrderingTerm where QueryOutput == Void {
+  public var _orderingTerm: OrderingTerm {
+    OrderingTerm(base: self)
+  }
+}
+
+public func raw<QueryOutput>(
+  _ queryFragment: QueryFragment,
+  as output: QueryOutput.Type = QueryOutput.self
+) -> RawQueryExpression<QueryOutput> {
+  RawQueryExpression(queryFragment: queryFragment)
+}
