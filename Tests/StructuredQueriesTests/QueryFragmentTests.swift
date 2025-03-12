bace91a4d8bf642d8f42a7422831d624239e13ee
Stephen Celis <stephen@stephencelis.com>
2025-03-12 00:25:34 -0700

    wip

--- Tests/StructuredQueriesTests/QueryFragmentTests.swift
+++ Tests/StructuredQueriesTests/QueryFragmentTests.swift
@@ -5,13 +5,43 @@ import Testing
 
 extension SnapshotTests {
   struct QueryFragmentTests {
-    @Test func snapshotting() {
+    @Test func string() {
       assertInlineSnapshot(
         of: RawQueryExpression("'What''s the point?'", as: String.self),
         as: .sql
       ) {
         """
         'What''s the point?'
+        """
+      }
+    }
+    @Test func identifier() {
+      assertInlineSnapshot(
+        of: RawQueryExpression(#""What's the point?""#, as: String.self),
+        as: .sql
+      ) {
+        """
+        "What's the point?"
+        """
+      }
+    }
+    @Test func brackets() {
+      assertInlineSnapshot(
+        of: RawQueryExpression("[What's the point?]", as: String.self),
+        as: .sql
+      ) {
+        """
+        [What's the point?]
+        """
+      }
+    }
+    @Test func backticks() {
+      assertInlineSnapshot(
+        of: RawQueryExpression("`What's the point?`", as: String.self),
+        as: .sql
+      ) {
+        """
+        `What's the point?`
         """
       }
     }
