dd7687316d1e1538b21036358624d4419cc02256
Brandon Williams <mbrandonw@hey.com>
2025-03-14 16:35:12 -0700

    wip

--- Tests/StructuredQueriesTests/JoinTests.swift
+++ Tests/StructuredQueriesTests/JoinTests.swift
@@ -5,35 +5,31 @@ import Testing
 
 extension SnapshotTests {
   @Suite struct JoinTests {
-    @Test func basics() {
-      assertInlineSnapshot(
-        of: SyncUp.join(Attendee.all()) { $0.id.eq($1.syncUpID) }
-          .order { syncUps, _ in syncUps.createdAt.desc() },
-        as: .sql
+    @Test func basics() throws {
+      try assertQuery(
+        Reminder.join(RemindersList.all()) { $0.remindersListID.eq($1.id) }
+          .order { reminders, _ in reminders.date.desc() }
+          .select { ($0.title, $1.name) }
       ) {
         """
-        SELECT "syncUps"."id", "syncUps"."duration", "syncUps"."title", "syncUps"."isActive", "syncUps"."createdAt", "attendees"."id", "attendees"."syncUpID", "attendees"."name", "attendees"."createdAt" FROM "syncUps" JOIN "attendees" ON ("syncUps"."id" = "attendees"."syncUpID") ORDER BY "syncUps"."createdAt" DESC
+        SELECT "reminders"."title", "remindersLists"."name" FROM "reminders" JOIN "remindersLists" ON ("reminders"."remindersListID" = "remindersLists"."id") ORDER BY "reminders"."date" DESC
+        """
+      }results: {
+        """
+        ┌────────────────────────────┬────────────┐
+        │ "Take out trash"           │ "Family"   │
+        │ "Pick up kids from school" │ "Family"   │
+        │ "Call accountant"          │ "Business" │
+        │ "Groceries"                │ "Personal" │
+        │ "Doctor appointment"       │ "Personal" │
+        │ "Haircut"                  │ "Personal" │
+        │ "Get laundry"              │ "Family"   │
+        │ "Send weekly emails"       │ "Business" │
+        │ "Take a walk"              │ "Personal" │
+        │ "Buy concert tickets"      │ "Personal" │
+        └────────────────────────────┴────────────┘
         """
       }
     }
   }
-}
-
-@Table
-private struct SyncUp {
-  let id: Int
-  var duration: Int
-  var title: String
-  var isActive: Bool
-  @Column(as: Date.ISO8601Representation.self)
-  var createdAt: Date
-}
-
-@Table
-private struct Attendee {
-  let id: Int
-  var syncUpID: Int
-  var name: String
-  @Column(as: Date.ISO8601Representation.self)
-  var createdAt: Date
 }
