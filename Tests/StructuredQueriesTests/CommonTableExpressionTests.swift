6027f4ad7d251fd1f2de67d7f1d23d4a8ca8f4ba
Stephen Celis <stephen@stephencelis.com>
2025-03-24 23:55:50 -0700

    wip

--- Tests/StructuredQueriesTests/CommonTableExpressionTests.swift
+++ Tests/StructuredQueriesTests/CommonTableExpressionTests.swift
@@ -30,6 +30,46 @@ extension SnapshotTests {
         """
       }
     }
+
+    @Test func insert() {
+      assertQuery(
+        With {
+          Reminder
+            .where { !$0.isCompleted }
+            .select { IncompleteReminder.Columns(isFlagged: $0.isFlagged, title: $0.title) }
+        } query: {
+          Reminder.insert {
+            ($0.remindersListID, $0.title, $0.isFlagged, $0.isCompleted)
+          } select: {
+            IncompleteReminder
+              .join(Reminder.all()) { $0.title.eq($1.title) }
+              .select { ($1.remindersListID, $0.title, !$0.isFlagged, true) }
+              .limit(1)
+          }
+          .returning(\.self)
+        }
+      ) {
+        """
+        WITH "incompleteReminders" AS (SELECT "reminders"."isFlagged" AS "isFlagged", "reminders"."title" AS "title" FROM "reminders" WHERE NOT ("reminders"."isCompleted")) INSERT INTO "reminders" ("remindersListID", "title", "isFlagged", "isCompleted") SELECT "reminders"."remindersListID", "incompleteReminders"."title", NOT ("incompleteReminders"."isFlagged"), 1 FROM "incompleteReminders" JOIN "reminders" ON ("incompleteReminders"."title" = "reminders"."title") LIMIT 1 RETURNING "reminders"."id", "reminders"."assignedUserID", "reminders"."date", "reminders"."isCompleted", "reminders"."isFlagged", "reminders"."notes", "reminders"."priority", "reminders"."remindersListID", "reminders"."title"
+        """
+      } results: {
+        """
+        ┌────────────────────────┐
+        │ Reminder(              │
+        │   id: 11,              │
+        │   assignedUserID: nil, │
+        │   date: nil,           │
+        │   isCompleted: true,   │
+        │   isFlagged: true,     │
+        │   notes: "",           │
+        │   priority: nil,       │
+        │   remindersListID: 1,  │
+        │   title: "Groceries"   │
+        │ )                      │
+        └────────────────────────┘
+        """
+      }
+    }
   }
 }
 
