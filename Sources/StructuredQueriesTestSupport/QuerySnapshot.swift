1d10f6f310293ebe79a609b8068718aecc49a68a
Stephen Celis <stephen@stephencelis.com>
2025-03-31 23:12:11 -0700

    wip

--- Sources/StructuredQueriesTestSupport/QuerySnapshot.swift
+++ Sources/StructuredQueriesTestSupport/QuerySnapshot.swift
@@ -1,17 +1,15 @@
 import SnapshotTesting
-import StructuredQueries
+import StructuredQueriesCore
 import Testing
 
-@MainActor @Suite(.serialized, .snapshots(record: .failed)) struct SnapshotTests {}
-
 extension Snapshotting where Value: Statement {
-  static var sql: Snapshotting<Value, String> {
+  public static var sql: Snapshotting<Value, String> {
     SimplySnapshotting.lines.pullback(\.query.debugDescription)
   }
 }
 
 extension Snapshotting where Value: QueryExpression {
-  static var sql: Snapshotting<Value, String> {
+  public static var sql: Snapshotting<Value, String> {
     SimplySnapshotting.lines.pullback(\.queryFragment.debugDescription)
   }
 }
